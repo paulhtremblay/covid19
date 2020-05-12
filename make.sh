abec52e95a879fc31ef4239501938ea7ad543731
Henry <paulhtremblay@gmail.com>
2020-05-11 23:20:33 -0700

    code for dev env

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 set -e
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 cp -R templates/styles html_temp/styles
 rm html_temp/* 2> /dev/null || echo 
 rm html_temp/states/* 2> /dev/null || echo
@@ -7,8 +8,14 @@ python us_states_rates.py
 python washington_state.py
 python make_territories.py
 python make_index_404.py
+if [ $BRANCH == 'development' ]; then
+	python upload_html_to_bucket_s3.py --branch dev
+
+elif [ $BRANCH == 'development' ]; then
+	python upload_html_to_bucket_s3.py --branch prod
+else
+	echo Not uploading because not dev or prod
+fi
 # copy styles
-#gsutil web set -m index.html -e 404.html gs://www.paulhtremblay.com
-python  upload_html_to_bucket.py
 
 
--- upload_html_to_bucket_s3.py
+++ upload_html_to_bucket_s3.py
@@ -1,6 +1,15 @@
 import boto3
 import os
 import glob
+import argparse
+
+def _get_args():
+    parser = argparse.ArgumentParser(description='upload')
+    parser.add_argument('--verbose', '-verbose', action='store_true', help='Oracle base table name')
+    parser.add_argument('--branch', '-b', choices = ['dev', 'prod'], required = True,
+            help='branch of git')
+    return parser.parse_args()
+
 
 
 def upload_to_storage(local_path, bucket_name, object_name):
@@ -10,7 +19,6 @@ def upload_to_storage(local_path, bucket_name, object_name):
     :param bucket: Bucket to upload to
     :param object_name: S3 object name. 
     """
-    return
     with open(local_path, 'rb') as read_obj:
         b = b''.join(read_obj.readlines())
     s3 = boto3.resource('s3')
@@ -21,7 +29,7 @@ def upload_to_storage(local_path, bucket_name, object_name):
         CacheControl='max-age=60, public',
         ) 
 
-def upload_html(bucket_name = 'seattle-data.com'):
+def upload_html(bucket_name ):
     files = glob.glob('html_temp/*.html')
     for i in files:
         head, tail = os.path.split(i)
@@ -33,12 +41,15 @@ def upload_html(bucket_name = 'seattle-data.com'):
         upload_to_storage(local_path = i, bucket_name = bucket_name,
                 object_name = 'states/{name}'.format(name = tail))
 
-if __name__ == '__main__':
-    upload_html()
+def get_bucket_name(branch):
+    if branch == 'dev':
+        return 'seattle-data-dev'
+    elif branch == 'prod':
+        raise NotImplementedError()
+    else:
+        raise ValueError('no if')
 
-"""
 if __name__ == '__main__':
-    file_name = '/home/henry/Downloads/test_upload.html'
-    bucket = 'seattle-data.com'
-    upload_file(file_name, bucket, object_name='test_upload3.html')
-"""
+    args = _get_args()
+    upload_html(get_bucket_name(args.branch))
+
