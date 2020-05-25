a815097ff296e511cbc24461fd724f9561827ad9
Brian Tremblay <dev@btrem.com>
2020-05-25 11:35:06 -0700

    Adds about.html page with list of project contributors
    Adds make_about.py, which retrieves json object from api.github.com with list of contributors, saves it as a dict, and passes it to about.html template which displays it as an html list element. Changes base.html template’s `<address>` element to link to the new page. Adds call to python make_about.py to make.sh.

---
+++ make_about.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+import ssl
+import urllib.request
+import json
+
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+with urllib.request.urlopen("https://api.github.com/repos/paulhtremblay/covid19/contributors") as url:
+    contributors = json.loads(url.read().decode())
+
+ENV = Environment(
+    loader=FileSystemLoader(os.path.join(
+        os.path.split(os.path.abspath(__file__))[0], 
+        'templates')),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+def make_about():
+    t = ENV.get_template('about.html')
+    html = t.render(title = 'about', 
+            date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            page_title = 'About this site',
+            contributors = contributors
+            )
+    with open(os.path.join('html_temp', 'about.html'), 'w') as write_obj:
+        write_obj.write(html)
+
+if __name__ == '__main__':
+    make_about()
