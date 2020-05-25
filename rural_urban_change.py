1c7fcba2239273ab6cf654bc6f88013afec33876
Henry <paulhtremblay@gmail.com>
2020-05-25 14:10:32 -0700

    adding functionality with bokeh

--- rural_urban_change.py
+++ rural_urban_change.py
@@ -3,6 +3,10 @@ from google.cloud import bigquery
 # from matplotlib.pyplot import plot_date, axis, show, gcf, bar
 import matplotlib.pyplot as plt
 
+from bokeh.io import show
+from bokeh.plotting import figure
+from bokeh.layouts import gridplot
+
 
 def get_data():
 
@@ -13,8 +17,8 @@ def get_data():
             t3.ru_code,
             t3.daily_cases,
             t3.daily_deaths,
-            --round(t3.daily_cases/t3.population *100000,2) as daily_cases_per_100k_people,
-            --round(t3.daily_deaths/t3.population *100000,2) as daily_deaths_per_100k_people
+            round(t3.daily_cases/t3.population *100000,2) as daily_cases_per_100k_people,
+            round(t3.daily_deaths/t3.population *100000,2) as daily_deaths_per_100k_people
 
     from(
 
@@ -53,18 +57,30 @@ def get_data():
     date_r = []
     val_u = []
     val_r = []
+    daily_cases_per_100k_people_u = []
+    daily_cases_per_100k_people_r = []
+    daily_deaths_per_100k_people_u = []
+    daily_deaths_per_100k_people_r = []
 
     for i in result:
         date = i.get('date')
         ru_code = i.get('ru_code')
         daily_cases = i.get('daily_cases')
+        daily_cases_per_100k_people = i.get('daily_cases_per_100k_people')
+        daily_deaths_per_100k_people = i.get('daily_deaths_per_100k_people')
         if ru_code == "U":
             date_u.append(date)
             val_u.append(daily_cases)
+            daily_cases_per_100k_people_u.append(daily_cases_per_100k_people)
+            daily_deaths_per_100k_people_u.append(daily_deaths_per_100k_people)
         else:
             date_r.append(date)
             val_r.append(daily_cases)
-    return (date_u, val_u, date_r, val_r)
+            daily_cases_per_100k_people_r.append(daily_cases_per_100k_people)
+            daily_deaths_per_100k_people_r.append(daily_deaths_per_100k_people)
+    return (date_u, val_u, date_r, val_r, daily_cases_per_100k_people_u , 
+            daily_cases_per_100k_people_r, daily_deaths_per_100k_people_u, 
+            daily_deaths_per_100k_people_r) 
 
 
 def daily_cases_graph(dates_1, val_1, dates_2, val_2):
@@ -75,10 +91,28 @@ def daily_cases_graph(dates_1, val_1, dates_2, val_2):
     #plt.dates('date')
     #plt.daily_cases('daily cases')
     #plt.setp(lines, color='r', linewidth=2.0)
-    plt.figure(figsize=(200, 150))
+    #plt.figure(figsize=(200, 150))
     plt.gcf().autofmt_xdate()
     plt.show()
+    plt.savefig('temp.png')
+
+def daily_cases_graph_bokeh(dates_1, val_1, dates_2, val_2):
+    #have to convert date to datetime. rather inconvenient but necessary
+    dates_1 = [datetime.datetime(x.year, x.month, x.day) for x in dates_1]
+    dates_2 = [datetime.datetime(x.year, x.month, x.day) for x in dates_2]
+    p = figure(x_axis_type = 'datetime', title = 'Urban vs Rural') 
+    p.line(x = dates_1, y = val_1, line_width = 2, color = 'red', legend_label = 'urban')
+    p.line(x = dates_2, y = val_2, line_width = 2, color = 'green', legend_label = 'rural')
+    p.yaxis.axis_label = 'cases/100k'
+    show(p)
 
 
-date_u, val_u, date_r, val_r = get_data()
+date_u, val_u, date_r, val_r,\
+    daily_cases_per_100k_people_u,\
+    daily_cases_per_100k_people_r,\
+    daily_deaths_per_100k_people_u,\
+    daily_deaths_per_100k_people_r\
+    = get_data()
 daily_cases_graph(date_u, val_u, date_r, val_r)
+#daily_cases_graph(date_u, daily_cases_per_100k_people_u, date_r, daily_cases_per_100k_people_r)
+daily_cases_graph_bokeh(date_u, daily_cases_per_100k_people_u, date_r, daily_cases_per_100k_people_r)
